20027f92fcba8c2c3bbfc8802a9ad417a7aac5db
anushka <maheshdesilva89@gmail.com>
2015-06-11 00:35:25 +0530

    adding webservie

--- www/js/service.js
+++ www/js/service.js
@@ -1,9 +1,9 @@
 angular.module('starter.services', [])
-    .constant("host", "http://192.168.123.168/")
-    // .constant("host", "")
+    // .constant("host", "http://192.168.123.168/")
+    .constant("host", "")
 
 .factory("Courier", function($http, $ionicLoading, host) {
-    var addDeliveryURL = host + "tadhack/geo-map/api/index.php";
+    var addDeliveryURL = host + "/application/tadhack/api/index.php";
     var candidateUpdateRequest = {
         method: 'GET',
         url: addDeliveryURL,
@@ -12,9 +12,9 @@ angular.module('starter.services', [])
     return {
         linktest: function() {
             return $http(candidateUpdateRequest).then(function(response) {
-                alert(response);
+                console.log(response);
             }).catch(function(response) {
-                alert("error" + response);
+                console.log(response);
             });
         }
     }
